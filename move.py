5501aa88d3b8cd2ba1fe60c41b550a6dfd94ca71
tilakamichael <mdt53229@uga.edu>
2022-04-28 18:13:51 -0400

    Organized the folder thru a python file

---
+++ move.py
@@ -0,0 +1,34 @@
+#Organizes the image folder to groups by species
+
+import csv
+import os
+import shutil
+
+file = open('train.csv')
+
+csvreader = csv.reader(file)
+
+header = []
+header = next(csvreader)
+print(header)
+
+images = []
+species = []
+
+for image in csvreader:
+    images.append(image[0])
+    species.append(image[1])
+
+for i in range (3):
+    print (images[i])
+    
+list = os.listdir("256x256_NormalizedWhaleImages")
+    
+for file in list:
+    path = os.path.join("256x256_NormalizedWhaleImages", species[images.index(file)])
+    if (species[images.index(file)] not in os.listdir("256x256_NormalizedWhaleImages")):        
+        os.makedirs(path)
+    
+    currPath = os.path.join("256x256_NormalizedWhaleImages", file)
+    shutil.move(currPath, path)
+    
